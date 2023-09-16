86489161b20a5482937b1bc8cfa59673110e1097
Ankireddy99 <20kb1a05d2@nbkrist.org>
2023-09-16 09:59:59 +0530

    add cors middleware

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const todayDate = new Date();
 console.log(todayDate); */
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
+app.use(cors());
+
 app.get("/",(request,response)=>{
     response.send("Hello Anki");
 })
